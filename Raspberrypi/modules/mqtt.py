e2944531ef4f56309a6b95e4585f75527ea29c6a
Tuguberk <tugberk2004@gmail.com>
2019-08-22 13:30:24 +0300

    Mqtt ayarlandı

--- Raspberrypi/modules/mqtt.py
+++ Raspberrypi/modules/mqtt.py
@@ -1,34 +1,15 @@
 import paho.mqtt.client as mqtt
-"""
-class mqttsunucu:
-    def __init__(self,link:str):
-        self.link = link
-        self.istemci = mqtt.Client("p1")
-        self.istemci.on_connect = mqttsunucu.baglanti_saglandiginda
-        self.istemci.on_message = mqttsunucu.mesaj_geldiginde
-        self.istemci.connect("iot.eclipse.org",1883,60)
-        self.istemci.loop_start()
-    
-    def baglanti_saglandiginda(client,userdata,flags,rc):
-        print("Sunucuya Bağlanıldı.")
-        self.istemci.subscribe(self.link)
-    
-    def mesaj_geldiginde(client,userdata,msg):
-        return msg.topic+" "+str(msg.payload)
-
-d = mqttsunucu("/robot")
-"""
-
 
 def baglanti_saglandiginda(client,userdata,flags,rc):
     print("Sunucuya Bağlanıldı.")
     istemci.subscribe("/robotaksi")
     
 def mesaj_geldiginde(client,userdata,msg):
-    print( msg.topic+" "+str(msg.payload))
+    print(str(msg.payload))
+    
 
 istemci = mqtt.Client("p1")
 istemci.on_connect = baglanti_saglandiginda
 istemci.on_message = mesaj_geldiginde
-istemci.connect("iot.eclipse.org",1883,60)
+istemci.connect("mqtt.eclipse.org",1883,60)
 istemci.loop_forever()
